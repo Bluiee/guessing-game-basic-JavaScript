696f8901650bd89d4baf6ed27b820ff3bb60748b
Bluie <60061812+Bluiee@users.noreply.github.com>
2020-06-06 17:45:26 +0100

    Create guessingVersion1.js
    A basic number guessing game in JavaScript

---
+++ guessingVersion1.js
@@ -0,0 +1,16 @@
+var secretNumber = 228;
+
+var stringGuess = prompt("Guess a number");
+var guess = Number(stringGuess);
+
+if(guess === secretNumber) {
+	alert("YOU GOT IT RIGHT!");
+}
+
+else if(guess > secretNumber){
+	alert("Too high. Guess again!");
+}
+
+else {
+	alert("Too low. Guess again!");
+}
